17773c26f325ced6c212247b968837432c8c8e09
AmoJava <Ph.ahmedmohsin@gmail.com>
2019-05-27 17:22:51 +0200

    Navigators routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 //27/5/2019
 //بسم الله الرحمن الرحيم
 
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 
 import 'package:playground_user/auth/Login.dart';
 import 'package:playground_user/playgroundlist.dart';
@@ -10,19 +10,17 @@ import 'Payment.dart';
 import 'Reservationpage.dart';
 import 'auth/Wellcome.dart';
 import 'auth/Signup.dart';
-    
-    void main(){
+
+void main() {
   runApp(MaterialApp(
     home: Wellcome(),
     routes: {
-      Login.id:(context)=>Login(),
-      SignUp.id:(context)=> SignUp(),
-      PlayGroundList.id:(context)=>PlayGroundList(),
-      ReservationPage.id : (context)=>ReservationPage(),
-      Confirmation.id:(context)=>Confirmation(),
-      Payment.id:(context)=>Payment(),
-
+      Login.id: (context) => Login(),
+      SignUp.id: (context) => SignUp(),
+      PlayGroundList.id: (context) => PlayGroundList(),
+      ReservationPage.id: (context) => ReservationPage(),
+      Confirmation.id: (context) => Confirmation(),
+      Payment.id: (context) => Payment(),
     },
   ));
-    }
-
+}
--- lib/playgroundlist.dart
+++ lib/playgroundlist.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
 
+import 'Reservationpage.dart';
+
 class PlayGroundList extends StatefulWidget {
-  static const String id = "pglist" ;
+  static const String id = "pglist";
   @override
   _PlayGroundListState createState() => _PlayGroundListState();
 }
 
 class _PlayGroundListState extends State<PlayGroundList> {
+  List names = ["gam3a", "talkha", "ahly", "zamalek", "mansoura"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
+        body: Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: ListView(
+      children: <Widget>[
+          InkWell(child: PgItem(name: "Ahly "),onTap: (){
+            Navigator.pushNamed(context, ReservationPage.id);
+          },),
+          PgItem(name: "Zamalek"),
+          PgItem(name: "Elboshy"),
+          PgItem(name: "gam3a"),
+          PgItem(name: "talkha"),
+          PgItem(name: "drsata"),
+          PgItem(name: "zhor"),
+          PgItem(name: "ngoom"),
+          PgItem(name: "10 street"),
+          PgItem(name: "fifa"),
+          PgItem(name: "mansoura stadium"),
+      ],
+    ),
+        ));
+  }
+}
 
-        ],
+class PgItem extends StatelessWidget {
+  String name;
+  PgItem({this.name});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: Card(
+        elevation: 2,
+        color: Colors.green.shade300,
+        child: Center(child: Text("  $name")),
       ),
     );
   }
