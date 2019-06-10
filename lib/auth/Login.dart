3eeebbf9591c759547b4bf10f5e96e0fbf76d741
AmoJava <Ph.ahmedmohsin@gmail.com>
2019-06-10 21:38:44 +0200

    login redesign

--- lib/auth/Login.dart
+++ lib/auth/Login.dart
@@ -69,117 +69,135 @@ class _LoginState extends State<Login> {
     return WillPopScope(
       onWillPop: _onBackPressed,
       child: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.network(
-                    "https://media.giphy.com/media/l3vQWvLXw74jemGoU/giphy.gif",
-                    width: double.infinity,
-                    height: 230,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text("log in with ,, ", style: TextStyle(fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey),),
-                  SizedBox(
-                    height: 3,
-                  ),
-                  MaterialButton(
-                    onPressed: handleSignin,
-                    color: Colors.red,
-                    minWidth: 160,
-                    child: Text(
-                      'gmail',
-                      style: textstyle,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text('or'),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  /*
-                  FlatButton(
-                      onPressed: () {
-                        _signIn()
-                            .then((FirebaseUser user) => print(user))
-                            .catchError((e) => print(e));
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fitHeight, repeat: ImageRepeat.repeatX,
+                  image: NetworkImage(
+                    //"https://media.giphy.com/media/l3vQWvLXw74jemGoU/giphy.gif",)
+                      "https://media.giphy.com/media/ckXTbeFGygcMiuh8pr/giphy.gif")
+              )),
+          child: Container(width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Spacer(),
+                Container(
+                  width: 250,
+                  color: Colors.white54,
+                  child: Column(
 
-                      },
-                      child: Container(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Login by ",
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      MaterialButton(
+                        onPressed: handleSignin,
                         color: Colors.red,
+                        minWidth: 160,
                         child: Text(
-                          'facebook',
-                          style: textstyle,
+                          'gmail',
+                          style: TextStyle(fontSize: 25, color: Colors.white),
                         ),
-                      )),*/
-                  MaterialButton(
-                    minWidth: 160,
-                    onPressed: () {
-                      fblogin.logInWithReadPermissions(
-                          ['email', 'public_profile']).then((result) {
-                        switch (result.status) {
-                          case FacebookLoginStatus.loggedIn:
-                            AuthCredential credential =
-                            FacebookAuthProvider.getCredential(
-                                accessToken: result.accessToken.token);
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text('or'),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      /*
+                      FlatButton(
+                          onPressed: () {
+                            _signIn()
+                                .then((FirebaseUser user) => print(user))
+                                .catchError((e) => print(e));
 
-                            FirebaseAuth.instance
-                                .signInWithCredential(credential);
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => UserProfile()));
+                          },
+                          child: Container(
+                            color: Colors.red,
+                            child: Text(
+                              'facebook',
+                              style: textstyle,
+                            ),
+                          )),*/
+                      MaterialButton(
+                        minWidth: 160,
+                        onPressed: () {
+                          fblogin.logInWithReadPermissions(
+                              ['email', 'public_profile']).then((result) {
+                            switch (result.status) {
+                              case FacebookLoginStatus.loggedIn:
+                                AuthCredential credential =
+                                FacebookAuthProvider.getCredential(
+                                    accessToken: result.accessToken.token);
 
-                            FirebaseAuth.instance
-                                .signInWithCredential(credential);
-                            // TODO: Handle this case.
-                            break;
-                          case FacebookLoginStatus.cancelledByUser:
-                          // TODO: Handle this case.
-                            break;
-                          case FacebookLoginStatus.error:
-                          // TODO: Handle this case.
-                            break;
-                        }
-                      }).catchError((e) {
-                        print(e);
-                      });
-                    },
-                    color: Colors.blue,
-                    child: Text(
-                      'Facebook',
-                      style: textstyle,
-                    ),
+                                FirebaseAuth.instance
+                                    .signInWithCredential(credential);
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => UserProfile()));
+
+                                FirebaseAuth.instance
+                                    .signInWithCredential(credential);
+                                // TODO: Handle this case.
+                                break;
+                              case FacebookLoginStatus.cancelledByUser:
+                              // TODO: Handle this case.
+                                break;
+                              case FacebookLoginStatus.error:
+                              // TODO: Handle this case.
+                                break;
+                            }
+                          }).catchError((e) {
+                            print(e);
+                          });
+                        },
+                        color: Colors.blue,
+                        child: Text(
+                          'Facebook',
+                          style: TextStyle(fontSize: 25, color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                    ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  MaterialButton(
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: MaterialButton(
                     height: 20,
                     onPressed: () {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) => PgLogin()));
                     },
-                    color: Colors.green,
-                    minWidth: 160,
+                    color: Colors.grey,
+                    minWidth: 100,
                     child: Text(
-                      'playGround admin',
+                      ' Admins',
                       style: textstyle,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
